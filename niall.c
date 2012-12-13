82809f5ebec113e06eb21ee45ba4ee754dff6495
Alexander Rødseth <rodseth@gmail.com>
2012-12-13 01:28:44 +0100

    Trying to make go get work

--- niall.c
+++ niall.c
@@ -35,8 +35,40 @@
 #include <time.h>
 #include <stdarg.h>
 
-#include "niall.h"
+#ifndef NIALL_H_INCLUDED
+#define NIALL_H_INCLUDED
 
+/* Startup/shutdown code.
+*/
+void Niall_Init(void);
+void Niall_Free(void);
+
+/* Niall's main functions.
+*/
+void Niall_Learn(char *Buffer);
+void Niall_Reply(char *Buffer,int BufSize);
+
+/* Housekeeping functions.
+*/
+void Niall_NewDictionary(void);
+void Niall_ListDictionary(void);
+void Niall_SaveDictionary(char *file);
+void Niall_LoadDictionary(char *file);
+void Niall_CorrectSpelling(char *Original,char *Correct);
+
+/* Warnings, errors and output
+ */
+void Niall_Warning(char *fmt, ...);
+void Niall_Error(char *fmt, ...);
+void Niall_Print(char *fmt, ...);
+
+/* For calling the Go functions for output
+ */
+extern void niall_go_print( char *msg );
+extern void niall_go_warning( char *msg );
+extern void niall_go_error( char *msg );
+
+#endif
 
 /********************************************************************************
 * Definitions and structures							*
